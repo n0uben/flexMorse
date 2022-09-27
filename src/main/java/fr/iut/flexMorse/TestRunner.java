d76b9d748a8f7be9cb49de044c7ef61120cdb07b
DrYruo <oury.jordan@hotmail.com>
2022-09-27 17:01:42 +0200

    tiens bg

--- src/main/java/fr/iut/flexMorse/TestRunner.java
+++ src/main/java/fr/iut/flexMorse/TestRunner.java
@@ -1,7 +1,14 @@
 package fr.iut.flexMorse;
 
+import fr.iut.flexMorse.helper.FileHelper;
+
+import java.io.File;
+import java.nio.file.Files;
+
 public class TestRunner {
     public static void main(String[] args) {
-
+        String filePath = new File("").getAbsolutePath() + "\\" + "src\\main\\resources\\static\\morse.txt";
+        System.out.println(filePath);
+        FileHelper.getFileRowsToStringList(filePath);
     }
 }
--- src/main/java/fr/iut/flexMorse/helper/FileHelper.java
+++ src/main/java/fr/iut/flexMorse/helper/FileHelper.java
@@ -1,14 +1,32 @@
 package fr.iut.flexMorse.helper;
 
-import java.nio.file.Files;
+import fr.iut.flexMorse.liste.NoeudListe;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileHelper {
-    public static List<String> getFileRowsToStringList(Files file) {
-        List<String> fileRows = new ArrayList<>();
+    public static NoeudListe getFileRowsToStringList(String path) {
+        NoeudListe fileRows = new NoeudListe();
+        File fileToRead = new File(path);
+        String row;
+        try {
+            BufferedReader bf = new BufferedReader(new FileReader(fileToRead));
+            while ((row = bf.readLine()) != null) {
+                System.out.println(row);
+            }
 
-
+            bf.close();
+        } catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         return fileRows;
+    }
+
+    public static String createRelativePath(String relativePath) {
+        
     }
 }
