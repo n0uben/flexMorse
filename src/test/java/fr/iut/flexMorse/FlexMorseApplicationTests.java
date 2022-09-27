a88e1c9344437e3c9502e86908e218ce6f325146
n0uben <benjamin.noureux@gmail.com>
2022-09-27 16:01:11 +0200

    Le premier commit

---
+++ src/test/java/fr/iut/flexMorse/FlexMorseApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.iut.flexMorse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexMorseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
