4bfc00fd20e3eb362f9f6ee268d8af92f3f195c0
n0uben <benjamin.noureux@gmail.com>
2022-10-03 14:57:22 +0200

    ajout mock rest controller avec 2 endpoints

---
+++ src/main/java/fr/iut/flexMorse/controller/TraductionController.java
@@ -0,0 +1,18 @@
+package fr.iut.flexMorse.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/traduction")
+public class TraductionController {
+
+//    Endpoints pour de faux pour les tests front
+    @GetMapping("/morse/alphabet")
+    public String traductionMorseAlphabet() {
+        return "olalala cest le morse vers l'alphabet magnifique traduction";
+    }
+    @GetMapping("/alphabet/morse")
+    public String traductionAlphabetMorse() {
+        return "olalala cest l'alphabet vers le morse magnifique traduction";
+    }
+}
