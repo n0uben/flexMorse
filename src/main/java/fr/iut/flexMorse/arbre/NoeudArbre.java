e3afed3c0a2315ee87df9b862d9064d425d71832
DrYruo <oury.jordan@hotmail.com>
2022-10-04 15:50:10 +0200

    arbre morse fonctionnel

--- src/main/java/fr/iut/flexMorse/arbre/NoeudArbre.java
+++ src/main/java/fr/iut/flexMorse/arbre/NoeudArbre.java
@@ -1,19 +1,15 @@
 package fr.iut.flexMorse.arbre;
 
 public class NoeudArbre {
-
     private String lettre;
-
+    private NoeudArbre previous;
     private NoeudArbre gauchePoint;
     private NoeudArbre droiteTiret;
 
+    /**
+     * Ce constructeur est vide car le seul moyen pour initialiser ses attribut sera de passer par la méthode add()
+     */
     public NoeudArbre() {
-    }
-
-    public NoeudArbre(String lettre, NoeudArbre gauchePoint, NoeudArbre droiteTiret) {
-        this.lettre = lettre;
-        this.gauchePoint = gauchePoint;
-        this.droiteTiret = droiteTiret;
     }
 
     public String getLettre() {
@@ -22,6 +18,14 @@ public class NoeudArbre {
 
     public void setLettre(String lettre) {
         this.lettre = lettre;
+    }
+
+    public NoeudArbre getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(NoeudArbre previous) {
+        this.previous = previous;
     }
 
     public NoeudArbre getGauchePoint() {
@@ -39,6 +43,50 @@ public class NoeudArbre {
     public void setDroiteTiret(NoeudArbre droiteTiret) {
         this.droiteTiret = droiteTiret;
     }
+
+    // Methods
+    public NoeudArbre getFirstNoeud() {
+        if (getPrevious() == null)
+            return this;
+        else return getPrevious().getFirstNoeud();
+    }
+
+    public void add(String lettre, String codeMorse) {
+        NoeudArbre noeud = this.getFirstNoeud();
+        // On boucle sur la chaine de caractere mais on s'arrête avant son dernier indice car celui-ci définira la lettre que contient le noeud
+        for (int i = 0; i < codeMorse.length() - 1; i++) {
+            String character = String.valueOf(codeMorse.charAt(i));
+            noeud = switchAndGenerate(noeud, character);
+        }
+        // On s'occupe ici du dernier noeud auquel on va attribuer la lettre
+        String lastCharacter = String.valueOf(codeMorse.charAt(codeMorse.length()-1));
+        noeud = switchAndGenerate(noeud, lastCharacter);
+        noeud.setLettre(lettre);
+    }
+
+    private NoeudArbre switchAndGenerate(NoeudArbre noeud, String characterToSwitch) {
+        switch (characterToSwitch) {
+            case "." -> {
+                if (noeud.getGauchePoint() == null) {
+                    noeud.setGauchePoint(new NoeudArbre());
+                    noeud.getGauchePoint().setPrevious(noeud);
+                }
+                return noeud.getGauchePoint();
+
+            }
+            case "-" -> {
+                if (noeud.getDroiteTiret() == null) {
+                    noeud.setDroiteTiret(new NoeudArbre());
+                    noeud.getDroiteTiret().setPrevious(noeud);
+                }
+                return noeud.getDroiteTiret();
+            }
+            default ->
+                    throw new IllegalArgumentException("Erreur, le code morse contient : " + characterToSwitch + " ce qui n'est pas un caractere valide");
+        }
+    }
+
+    
 
     @Override
     public String toString() {
