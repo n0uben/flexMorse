01d8f78c866a278d31f3796a5ec0a2d2f84f0ca2
DrYruo <oury.jordan@hotmail.com>
2022-11-15 15:20:48 +0100

    Ajout des services de conversion

--- src/main/java/fr/iut/flexMorse/traduction/TraductionService.java
+++ src/main/java/fr/iut/flexMorse/traduction/TraductionService.java
@@ -1,12 +1,39 @@
 package fr.iut.flexMorse.traduction;
 
+import fr.iut.flexMorse.arbre.NoeudArbre;
+import fr.iut.flexMorse.helper.FileHelper;
+import fr.iut.flexMorse.liste.NoeudListe;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TraductionService {
 
     public String morseVersAlphabet(String texteATraduire) {
-        return "";
+        String path = FileHelper.createAbsolutPathToRessources("morse.txt");
+        NoeudArbre arbreConversionMorseLetrre = FileHelper.getFileRowsToStringNoeudArbre(path);
+
+        StringBuilder messageTraduit = new StringBuilder();
+        String[] texteATraduireList = texteATraduire.trim().split(" ");
+
+        for (String code : texteATraduireList) {
+            messageTraduit.append(arbreConversionMorseLetrre.getLettreByCode(code));
+        }
+
+        return messageTraduit.toString();
     }
 
     public String alphabetVersMorse(String texteATraduire) {
-        return "";
+        String path = FileHelper.createAbsolutPathToRessources("morse.txt");
+        NoeudListe listConversionLettreMorse = FileHelper.getFileRowsToStringNoeudListe(path);
+
+        StringBuilder messageTraduit = new StringBuilder();
+        String[] texteATraduireList = texteATraduire.trim().split(" ");
+
+        for (String lettre : texteATraduireList) {
+            messageTraduit.append(listConversionLettreMorse.getCodeByLettre(lettre));
+        }
+        
+        return messageTraduit.toString();
     }
 }
