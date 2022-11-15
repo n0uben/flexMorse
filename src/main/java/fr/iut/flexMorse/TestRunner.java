511421193605826099058c2571b3561bab178969
DrYruo <oury.jordan@hotmail.com>
2022-11-15 16:30:27 +0100

    suppression des sout

--- src/main/java/fr/iut/flexMorse/TestRunner.java
+++ src/main/java/fr/iut/flexMorse/TestRunner.java
@@ -7,14 +7,6 @@ import fr.iut.flexMorse.traduction.TraductionService;
 
 public class TestRunner {
     public static void main(String[] args) {
-        String path = FileHelper.createAbsolutPathToRessources("morse.txt");
-        NoeudListe listConversionLettreMorse = FileHelper.getFileRowsToStringNoeudListe(path);
-        NoeudArbre arbreConversionMorseLetrre = FileHelper.getFileRowsToStringNoeudArbre(path);
-
-        String test = "...";
-
-        System.out.println(arbreConversionMorseLetrre.getLettreByCode(test));
-        System.out.println(listConversionLettreMorse.getCodeByLettre("S"));
         TraductionService trad = new TraductionService();
         System.out.println(trad.alphabetVersMorse("blabla et tata"));
         System.out.println(trad.morseVersAlphabet("-... .-.. .- -... .-.. .- /. -/- .- - .-"));
